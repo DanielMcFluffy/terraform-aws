5bfb12023a410358396bb3182d6eb3ffeb6f0eab
DanielMcfluffy <daniel001.dhf@gmail.com>
2025-02-23 23:54:01 +0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ami" {
+  owners = ["137112412989"] #aws owner id
+  most_recent = true
+
+}
+
